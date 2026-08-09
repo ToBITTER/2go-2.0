62a915266662e988b9ce69572a25839a3ab7f9d6
ToBITTER <tobiloba.gbenle@gmail.com>
2026-08-09 13:00:33 +0100

    Phase 1: Authentication/Profiles

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -99,6 +99,15 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
+// Validate ../../src/app/profile/[username]/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/profile/[username]">> = Specific
+  const handler = {} as typeof import("../../src/app/profile/[username]/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../src/app/profile/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/profile">> = Specific
